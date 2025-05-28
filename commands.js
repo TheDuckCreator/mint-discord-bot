8efec413433e63f3cf3bd5ecd504c786d46a4d2d
Tin Theethawat Savastham <theethawat.s@outlook.com>
2025-05-28 23:39:01 +0700

    ✨ Mint can interaction

--- commands.js
+++ commands.js
@@ -1,6 +1,6 @@
-import 'dotenv/config';
-import { getRPSChoices } from './game.js';
-import { capitalize, InstallGlobalCommands } from './utils.js';
+import "dotenv/config";
+import { getRPSChoices } from "./game.js";
+import { capitalize, InstallGlobalCommands } from "./utils.js";
 
 // Get the game choices from game.js
 function createCommandChoices() {
@@ -19,22 +19,37 @@ function createCommandChoices() {
 
 // Simple test command
 const TEST_COMMAND = {
-  name: 'test',
-  description: 'Basic command',
+  name: "test",
+  description: "Basic command",
   type: 1,
   integration_types: [0, 1],
   contexts: [0, 1, 2],
 };
 
-// Command containing options
-const CHALLENGE_COMMAND = {
-  name: 'challenge',
-  description: 'Challenge to a match of rock paper scissors',
+const MINT_COMMAND = {
+  name: "mint",
+  description: "เรียกมิ้นท์",
+  type: 1,
+  integration_types: [0, 1],
+  contexts: [0, 1, 2],
   options: [
     {
       type: 3,
-      name: 'object',
-      description: 'Pick your object',
+      name: "มีอะไรอยากจะบอกเหรอ",
+      description: "มีอะไรอยากบอกมิ้นท์เหรอ",
+      required: true,
+    },
+  ],
+};
+// Command containing options
+const CHALLENGE_COMMAND = {
+  name: "challenge",
+  description: "Challenge to a match of rock paper scissors",
+  options: [
+    {
+      type: 3,
+      name: "object",
+      description: "Pick your object",
       required: true,
       choices: createCommandChoices(),
     },
@@ -44,6 +59,6 @@ const CHALLENGE_COMMAND = {
   contexts: [0, 2],
 };
 
-const ALL_COMMANDS = [TEST_COMMAND, CHALLENGE_COMMAND];
+const ALL_COMMANDS = [TEST_COMMAND, CHALLENGE_COMMAND, MINT_COMMAND];
 
 InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
---
+++ config/config.js
@@ -0,0 +1,15 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const config = {
+  openai: {
+    baseURL: "https://openrouter.ai/api/v1",
+    apiKey: process.env.OPEN_ROUTER_TOKEN,
+    mainModelName: "scb10x/llama3.1-typhoon2-8b-instruct",
+  },
+  inference: {
+    apiKey: process.env.HUGGING_FACE_TOKEN,
+  },
+};
+
+module.exports = config;
